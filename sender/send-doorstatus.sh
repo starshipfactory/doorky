bbd8f0d83e17c8261434ca385126deb8bdefaf16
Caoimhe Chaos <caoimhechaos@protonmail.com>
2016-08-04 02:47:02 +0200

    Fix success check.

--- sender/send-doorstatus.sh
+++ sender/send-doorstatus.sh
@@ -50,7 +50,7 @@ do
 		iv=$(openssl rand -hex 16)
 		hash="$(printf "$DOORNAME\n$newstate\n$ts" | openssl sha1 -sha256 -binary | openssl aes-256-cbc -iv "$iv" -kfile "$HOME/.doorky/secret" -md sha256 -e -base64 -nosalt -nopad | tr "+/" "-_" | tr -d "\n")"
 		ftp -i -o /dev/null "http://$DOORSTATUS_SERVER/api/doorstatus?door=$DOORNAME&val=$newstate&ts=$ts&hash=$hash&iv=$iv" > /dev/null 2>&1
-		if [ x"$?" = 0 ]
+		if [ x"$?" = x0 ]
 		then
 			oldstate="$newstate"
 		fi
